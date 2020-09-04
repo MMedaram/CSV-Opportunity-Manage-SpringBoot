7148c2e3e4e910e747d5a26184d04b56c3da7450
momedara <momedara@MOMEDARA-9P20E.cisco.com>
2020-09-04 06:47:46 -0700

    added datasource programatically

---
+++ src/main/java/com/example/core/config/DataSourceConfig.java
@@ -0,0 +1,45 @@
+package com.example.core.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(entityManagerFactoryRef = "entityManagerFactory",basePackages = {"com.example.core.repository"})
+@EnableTransactionManagement
+public class DataSourceConfig {
+    
+    @Bean
+    public DataSource getDataSource() {
+    	String driverClass="org.postgresql.Driver";
+    	String url="jdbc:postgresql://localhost:5432/emp";
+    	String userName="postgres";
+    	String password="postgres";
+    	
+    	DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(driverClass);
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(userName);
+        dataSourceBuilder.password(password);
+        return dataSourceBuilder.build();
+    }
+
+  /*
+   *   @Bean
+   
+    public DataSource getDataSource() {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("com.mysql.jdbc.Driver");
+        dataSourceBuilder.url("jdbc:mysql://localhost:3306/emp");
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password("root");
+        return dataSourceBuilder.build();
+    }
+   */
+    
+
+}
